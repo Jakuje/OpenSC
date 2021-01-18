51635b2ff8305bc1086b1f33412588d2e3ab966d
Doug Engert <deengert@gmail.com>
2021-01-17 21:12:12 -0600

    SM Cert Signer does not have pubkey/privkey on card.
     On branch PIV-4-extensions
     Changes to be committed:
    	modified:   pkcs15-piv.c

--- src/libopensc/pkcs15-piv.c
+++ src/libopensc/pkcs15-piv.c
@@ -341,7 +341,6 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	 */
 
 #define PIV_NUM_CERTS 25
-#define PIV_NUM_PUB_KEYS 25
 #define PIV_NUM_KEYS  24
 
 	// clang-format off
@@ -1008,7 +1007,7 @@ static int sc_pkcs15emu_piv_init(sc_pkcs15_card_t *p15card)
 	 * at a later time. The piv-tool can stash  pubkey in file
 	 */
 	sc_log(card->ctx,  "PIV-II adding pub keys...");
-	for (i = 0; i < PIV_NUM_PUB_KEYS; i++) {
+	for (i = 0; i < PIV_NUM_KEYS; i++) {
 		struct sc_pkcs15_pubkey_info pubkey_info;
 		struct sc_pkcs15_object     pubkey_obj;
 		struct sc_pkcs15_pubkey *p15_key = NULL;
