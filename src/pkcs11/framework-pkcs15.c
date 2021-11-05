3918518d9fbddd2afa2d908d28d4d51eca4bf41c
xhanulik <vhanulik@redhat.com>
2021-11-05 11:20:53 +0100

    Check for loaded fips provider with OSSL_PROVIDER_available()

--- src/pkcs11/framework-pkcs15.c
+++ src/pkcs11/framework-pkcs15.c
@@ -30,6 +30,9 @@
 #include "common/compat_strnlen.h"
 #ifdef ENABLE_OPENSSL
 #include <openssl/sha.h>
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#include <openssl/provider.h>
+#endif
 #else
 #define SHA_DIGEST_LENGTH	20
 #endif
@@ -6269,13 +6272,23 @@ register_mechanisms(struct sc_pkcs11_card *p11card)
 			if (rc != CKR_OK)
 				return rc;
 		}
-		if (!FIPS_mode() && rsa_flags & SC_ALGORITHM_RSA_HASH_MD5) {
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
+		if (!FIPS_mode()
+#else
+		if (!OSSL_PROVIDER_available(NULL, "fips")
+#endif
+			&& rsa_flags & SC_ALGORITHM_RSA_HASH_MD5) {
 			rc = sc_pkcs11_register_sign_and_hash_mechanism(p11card,
 				CKM_MD5_RSA_PKCS, CKM_MD5, mt);
 			if (rc != CKR_OK)
 				return rc;
 		}
-		if (!FIPS_mode() && rsa_flags & SC_ALGORITHM_RSA_HASH_RIPEMD160) {
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
+		if (!FIPS_mode()
+#else
+		if (!OSSL_PROVIDER_available(NULL, "fips")
+#endif
+			&& rsa_flags & SC_ALGORITHM_RSA_HASH_RIPEMD160) {
 			rc = sc_pkcs11_register_sign_and_hash_mechanism(p11card,
 				CKM_RIPEMD160_RSA_PKCS, CKM_RIPEMD160, mt);
 			if (rc != CKR_OK)
--- src/pkcs11/openssl.c
+++ src/pkcs11/openssl.c
@@ -34,6 +34,7 @@
 #if OPENSSL_VERSION_NUMBER >= 0x30000000L
 #include <openssl/core_names.h>
 #include <openssl/param_build.h>
+#include <openssl/provider.h>
 #endif
 #include "libopensc/sc-ossl-compat.h"
 #ifndef OPENSSL_NO_EC
@@ -273,7 +274,11 @@ sc_pkcs11_register_openssl_mechanisms(struct sc_pkcs11_card *p11card)
 	sc_pkcs11_register_mechanism(p11card, dup_mem(&openssl_sha384_mech, sizeof openssl_sha384_mech));
 	openssl_sha512_mech.mech_data = EVP_sha512();
 	sc_pkcs11_register_mechanism(p11card, dup_mem(&openssl_sha512_mech, sizeof openssl_sha512_mech));
+#if OPENSSL_VERSION_NUMBER < 0x30000000L
 	if (!FIPS_mode()) {
+#else
+	if (!OSSL_PROVIDER_available(NULL, "fips")) {
+#endif
 		openssl_md5_mech.mech_data = EVP_md5();
 		sc_pkcs11_register_mechanism(p11card, dup_mem(&openssl_md5_mech, sizeof openssl_md5_mech));
 		openssl_ripemd160_mech.mech_data = EVP_ripemd160();
