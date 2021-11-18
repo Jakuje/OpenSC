8ef35c4a0685802df18541c6e3f60256f849d68a
Doug Engert <deengert@gmail.com>
2021-11-18 13:25:22 -0600

    Piv duplicate sc_atr entry removed
    Remove duplicate entry in "sc_atr" for card that may support 800-73-4
    
    With 800-73-4 Secure Messaging the SELECT AID response specifies
    which cryptographic algorithms under tag 0xAC are supported for Secure Messaging.
    
    The code was using the discovery object to test if the PIV applet is active
    as some cards have a card issue of losing the login state if the SELECT AID
    is used instead. (None of these cards support SM so reading the discovey object
    was as good as doing SELECT_AID.)
    
    The problme was found while running in contactless mode, card would work the first time
    becaus the discovery object would not find the PIV applet so a SELECT AID was
    done and it would also update the the cryptographic algorithms.
    
    When run a second time, reading the discovery object would work but the SELECT AID
    would only be done near the end of match routine for card types the may support 800-73-4.
    The duplicate "sc_atr" was not listed as one the need to have SELECT AID done.
    
     Changes to be committed:
    	modified:   card-piv.c
    
     interactive rebase in progress; onto 076a8856
     Last commands done (15 commands done):
        pick 651d9911 PIV-4 improved pairing_code processing
        pick 420bd554 Piv duplicate sc_atr entry removed
     Next commands to do (10 remaining commands):
        pick ca482348 PIV change processing of CVC certificates
        pick a11256e9 PIV when built without OpenSSL or older versions
     You are currently rebasing branch 'PIV-4-extensions' on '076a8856'.
    
     Changes to be committed:
    	modified:   src/libopensc/card-piv.c

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -528,10 +528,6 @@ static const struct sc_atr_table piv_atrs[] = {
 	/* PIVKey uTrust FIDO2 (73) */
 	{ "3b:96:11:81:21:75:75:54:72:75:73:74:73", NULL, NULL, SC_CARD_TYPE_PIV_II_PIVKEY, 0, NULL },
 
-	/* ID-One PIV 2.4.1 on Cosmo V8.1 */
-	{ "3b:d6:96:00:81:b1:fe:45:1f:87:80:31:c1:52:41:1a:2a", NULL, NULL, SC_CARD_TYPE_PIV_II_OBERTHUR, 0, NULL },
-	{ "3b:86:80:01:80:31:c1:52:41:12:76", NULL, NULL, SC_CARD_TYPE_PIV_II_OBERTHUR, 0, NULL }, /* contactless */
-
 	/* Swissbit iShield Key Pro with PIV endpoint applet */
 	{ "3b:97:11:81:21:75:69:53:68:69:65:6c:64:05", NULL, NULL, SC_CARD_TYPE_PIV_II_SWISSBIT, 0, NULL },
 
